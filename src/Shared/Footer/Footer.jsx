c9acfaf013a929208bc7de6254cfd9e7bc4309e8
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-11-24 15:45:09 +0600

    added footer

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,5 +1,64 @@
+import { Button, Grid, Paper, Typography } from "@mui/material";
+import logo from "../../../public/logo-no-background.png"; // Replace with the actual path to your logo
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>footer</div>;
+  const login = true;
+  const footerLinks = [
+    "Home",
+    "Community",
+    "Blogs",
+    "About Us",
+    "Contact Us",
+    ...(login ? ["Login"] : []),
+    ...(login ? ["Register"] : []),
+  ];
+
+  return (
+    <Paper elevation={3} style={{ padding: "20px", marginTop: "20px" }}>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item textAlign={"center"}>
+          {/* Your Logo */}
+          <img
+            src={logo}
+            alt="Explore Elite"
+            style={{ width: "200px", height: "auto" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={2} justifyContent="center">
+            {footerLinks.map((link, index) => (
+              <Grid item key={index}>
+                <Button
+                  key={index}
+                  // onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={
+                    link === "Home"
+                      ? "/"
+                      : `/${link.toLowerCase().replace(/\s+/g, "-")}`
+                  }
+                  sx={{
+                    my: 2,
+                    color: "#666666",
+                    display: "block",
+                    "&:hover": { color: "#e65728" },
+                  }}
+                >
+                  {link}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" color="#666666" align="center">
+            © {new Date().getFullYear()} Your Company Name. All rights reserved.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
 
 export default Footer;
